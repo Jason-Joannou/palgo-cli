662f6be7ec7ad5fb639a2e1aa0832eebc6755a39
Jason Joannou <jjoannou@bscglobal.com>
2024-03-16 21:27:25 +0200

    CLI can now accept multiple argument
    - Specifies number of accounts to create/create and fund

--- palgo/generate_account/cli.py
+++ palgo/generate_account/cli.py
@@ -1,13 +1,15 @@
 import argparse
 from generate_account import generate_account
 
-def main(choice: str, to_file: bool) -> None:
+def main(choice: str, to_file: bool, n_accounts: int) -> None:
     if choice == "fund":
-        account = generate_account(to_file=to_file)
-        account.fund_address()
+        accounts = generate_account(to_file=to_file, n_accounts=n_accounts)
+        for account in accounts:
+            account.fund_address()
     else:
-        account = generate_account(to_file=to_file)
-        account.account_info()
+        accounts = generate_account(to_file=to_file, n_accounts=n_accounts)
+        for account in accounts:
+            account.account_info()
 
 
 if __name__ == "__main__":
@@ -19,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("generate", metavar="CHOICE", help="Generate a new account based on the specified choice. "
                                                             "Use 'Create' to create an account only, or 'Fund' to create "
                                                             "and fund the account.", type=str, choices=["Create", "Fund", "create", "fund"])
+    parser.add_argument("-m","--multiple", metavar="NumAccounts", type=int, help="Specifiy the number of accounts to generate", default=1)
     parser.add_argument("-wf", "--write-file", help="Write the contents of the accounts to a text file named 'test_accounts.txt'. By default %(prog)s writes to console",
                         action="store_true")
     parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.0.1")
@@ -28,4 +31,5 @@ if __name__ == "__main__":
 
     choice = args.generate.lower()
     to_file = args.write_file
-    main(choice=choice, to_file=to_file)
+    n_accounts =  args.multiple
+    main(choice=choice, to_file=to_file, n_accounts=n_accounts)
