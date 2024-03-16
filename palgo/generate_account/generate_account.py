9e07c82af2ac2902f136afee4f8e64c55b6ae029
Jason Joannou <jjoannou@bscglobal.com>
2024-03-16 21:27:50 +0200

    Updated generate account method to accept n_accounts argument

--- palgo/generate_account/generate_account.py
+++ palgo/generate_account/generate_account.py
@@ -2,6 +2,7 @@ from algosdk import account, encoding, mnemonic
 import os
 from accounts import Account
 from datetime import datetime
+from typing import List
 
 # TODO
 # Once account is created
@@ -17,16 +18,19 @@ class InvalidAddressError(Exception):
 def load_account(address: str, private_key: str, pass_phrase: str, to_file: bool) -> Account:
     return Account(address=address, private_key=private_key, pass_phrase=pass_phrase, to_file=to_file)
 
-def generate_account(to_file: bool) -> Account:
-    # generate an account
-    private_key, address = account.generate_account()
-    pass_phrase = mnemonic.from_private_key(private_key)
+def generate_account(to_file: bool, n_accounts: int) -> List[Account]:
+    accounts = []
+    for _ in range(1,n_accounts+1):
+        # generate an account
+        private_key, address = account.generate_account()
+        pass_phrase = mnemonic.from_private_key(private_key)
 
-    # check if the address is valid
-    if not encoding.is_valid_address(address):
-        raise InvalidAddressError()
-    
-    user_account = load_account(address=address,private_key=private_key,pass_phrase=pass_phrase,to_file=to_file)
-    user_account.write_message(message=f"Account successfuly created on {datetime.today()}")
-    return user_account
+        # check if the address is valid
+        if not encoding.is_valid_address(address):
+            raise InvalidAddressError()
+        
+        user_account = load_account(address=address,private_key=private_key,pass_phrase=pass_phrase,to_file=to_file)
+        user_account.write_message(message=f"Account successfuly created on {datetime.today()}")
+        accounts.append(user_account)
+    return accounts
 
