fc6c0bccb2c101d637b096e38aeaee7ecc9f6115
Jason Joannou <jjoannou@bscglobal.com>
2024-03-17 23:24:31 +0200

    Introduced new tests - split the tests up

--- palgo/generate_account/tests/test_generate_account.py
+++ palgo/generate_account/tests/test_generate_account.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import Mock, patch
-from ..accounts import Account
+from ...accounts import Account
 from ..generate_account import generate_account, InvalidAddressError
 
 @pytest.fixture
@@ -36,25 +36,46 @@ def test_generate_account(mock_algo_gen_account, mock_mnemonic, mock_encoding):
     wf = False
     n_accounts = 1
     test_pk, test_address = ("private_key","address")
-    test_mnemonic = "mnemonic"
 
     test_account = generate_account(to_file=wf,n_accounts=n_accounts)
     mock_algo_gen_account.assert_called_once()
     private_key, address = mock_algo_gen_account.return_value
     assert test_pk == private_key, f"Validation failed, test private key '{test_pk}' is not matching returned private key '{private_key}'"
     assert test_address == address, f"Validation failed, test address '{test_address}' is not matching returned address '{address}'"
+
+def test_mnemonic(mock_algo_gen_account, mock_mnemonic, mock_encoding):
+    # These asserts can be split into their own methods
+    wf = False
+    n_accounts = 1
+    test_mnemonic = "mnemonic"
+
+    test_account = generate_account(to_file=wf,n_accounts=n_accounts)
     mock_mnemonic.assert_called_once()
     mnemonic = mock_mnemonic.return_value
     assert test_mnemonic == mnemonic, f"Validation failed, test mnemonic '{test_mnemonic}' is not matching returned mnemonic '{mnemonic}'"
+
+
+def test_encoding(mock_algo_gen_account, mock_mnemonic, mock_encoding):
+
+    # These asserts can be split into their own methods
+    wf = False
+    n_accounts = 1
+
+    test_account = generate_account(to_file=wf,n_accounts=n_accounts)
+
     mock_encoding.assert_called_once()
-
-    assert isinstance(test_account, list), f"Validation failed, type of test account {type(test_account)} did not match list type"
-    assert isinstance(test_account[0], Account), f"Validation failed, type of test account {type(test_account)} did not match Account type"
-
     mock_encoding.return_value = False
-
     with pytest.raises(InvalidAddressError):
         test_account = generate_account(to_file=wf,n_accounts=n_accounts)
 
+def test_account_return(mock_algo_gen_account, mock_mnemonic, mock_encoding):
+
+    wf = False
+    n_accounts = 1
+
+    test_account = generate_account(to_file=wf,n_accounts=n_accounts)
+
+    assert isinstance(test_account, list), f"Validation failed, type of test account {type(test_account)} did not match list type"
+    assert isinstance(test_account[0], Account), f"Validation failed, type of test account {type(test_account)} did not match Account type"
 
 
