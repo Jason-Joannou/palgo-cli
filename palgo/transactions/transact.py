00df03033b0a32f81a6d996731d6bec2e6fa5a4b
Jason Joannou <jjoannou@bscglobal.com>
2024-03-25 09:48:25 +0200

    Added logging methods

--- palgo/transactions/transact.py
+++ palgo/transactions/transact.py
@@ -4,6 +4,7 @@ from base64 import b64decode
 import json
 from ..accounts import Account
 from typing import Dict
+import os
 
 class InsufficientFunds(Exception):
 
@@ -12,11 +13,12 @@ class InsufficientFunds(Exception):
 
 class Transact():
 
-    def __init__(self) -> None:
+    def __init__(self, to_file: bool = False) -> None:
         self.api_key = ''
         self.algod_address = "https://testnet-api.algonode.cloud"
         self.algod_client = algod.AlgodClient(self.api_key, self.algod_address)
         self.params = self.algod_client.suggested_params()
+        self.to_file = to_file
 
     def single_sig_transaction(self, sender_account: Account, receiver_account: Account, amount: float, note: str) -> Dict:
         # Have to make sure that sender address has cash to send, if not they must fund
@@ -36,12 +38,29 @@ class Transact():
 
         # submit the transaction and get back a transaction id
         txid = self.algod_client.send_transaction(signed_txn)
-        print("Successfully submitted transaction with txID: {}".format(txid))
+        self.write_message("Successfully submitted transaction with txID: {}".format(txid))
 
         # wait for confirmation
         txn_result = transaction.wait_for_confirmation(self.algod_client, txid)
 
-        print(f"Transaction information: {json.dumps(txn_result, indent=4)}")
-        print(f"Decoded note: {b64decode(txn_result['txn']['txn']['note'])}")
+        self.write_message(f"Transaction information: {json.dumps(txn_result, indent=4)}")
+        self.write_message(f"Decoded note: {b64decode(txn_result['txn']['txn']['note'])}")
 
         return txn_result
+    
+    def write_message(self,message: str) -> None:
+        if self.to_file:
+            self.write_to_file(message=message)
+        else:
+            self.write_to_console(message=message)
+
+    def write_to_file(self, message: str, file_name: str = "./test_accounts.txt") -> None:
+        mode = "w" if not os.path.exists(file_name) else "a"
+        with open(file_name, mode) as file:
+            file.write("\n" if mode == "a" else "")  # Add a newline before appending new data
+            file.write(message)
+        print("Data written to" if mode == "w" else "Data appended to", file_name)
+
+
+    def write_to_console(self, message: str) -> None:
+        print(message)
